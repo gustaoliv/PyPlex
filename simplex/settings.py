7696b5b30e1798f781e0d06e458c280e33dd3377
Gustavo Oliveira <gusta.oliv4@gmail.com>
2022-10-13 21:05:58 -0300

    configure template

--- simplex/settings.py
+++ simplex/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'simplex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
---
+++ static/js/main.js
@@ -0,0 +1,35 @@
+$(function(){
+    $("#form-total").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        autoFocus: true,
+        transitionEffectSpeed: 500,
+        titleTemplate : '<div class="title">#title#</div>',
+        labels: {
+            previous : 'Back',
+            next : 'Next',
+            finish : 'Confirm',
+            current : ''
+        },
+        onStepChanging: function (event, currentIndex, newIndex) { 
+            var fullname = $('#first_name').val() + ' ' + $('#last_name').val();
+            var room = $('#room').val();
+            var day = $('#day').val();
+            var time = $('#time').val();
+
+            $('#fullname-val').text(fullname);
+            $('#room-val').text(room);
+            $('#day-val').text(day);
+            $('#time-val').text(time);
+
+            return true;
+        }
+    });
+    $("#day").datepicker({
+        dateFormat: "MM - DD - yy",
+        showOn: "both",
+        buttonText : '<i class="zmdi zmdi-chevron-down"></i>',
+    });
+});
