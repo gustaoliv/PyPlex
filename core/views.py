8dcd1ef570068f415ebddb2512bf26cd020516c1
Gustavo Oliveira <gusta.oliv4@gmail.com>
2022-12-10 10:28:16 -0300

    housekeeping and add alert error

--- core/views.py
+++ core/views.py
@@ -1,17 +1,15 @@
 import json
-import pdb
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.shortcuts import render
+from plotly.offline import plot
 import simplex.main
 from .forms import *
 from .utils import *
-from django.shortcuts import redirect
-from django.http import JsonResponse
-from plotly.offline import plot
-import plotly.graph_objects as go
-import plotly.express as px
-import pandas as pd
-import numpy as np
-from simplex import *
+
 
 # Create your views here.
 def first_step(request):
@@ -23,7 +21,6 @@ def first_step(request):
         return redirect('/second-step')
     else:
         request.session.flush()
-
         form = FirstStepForm()
         return render(request, 'formulario.html', {'form': form})
 
@@ -45,7 +42,7 @@ def second_step(request):
 
         return redirect(url)
     else:
-       try:
+        try:
             numVar = int(request.session['numVar'])
             numRest = int(request.session['numRest'])
             form = SecondStepForm(numVar, numRest)
@@ -53,8 +50,9 @@ def second_step(request):
             return render(request, 'formulario2.html', {'form': form, 'numVar': range(numVar), 'numRest': range(numRest)
                 , 'classCol': f'col-sm-{int(10 / (numVar + 1))}', 'sliceRest': f'{1 + numVar}:'
                 , 'sliceObjet': f'1:{numVar + 1}'})
-       except:
-           return  redirect('/')
+        except:
+            messages.error(request, 'Algo de inesperado aconteceu. Verifique as entradas.')
+            return redirect('/')
 
 
 def third_step(request):
@@ -71,9 +69,11 @@ def tabular_view(request):
         json_response = simplex.main.solve_simplex(json_request)
         json_response = json.loads(json_response)
 
+        pdb.set_trace()
+
         headers = ["Base", "z"] + json_response["result"]["variables"] + ["b"]
         interactions = json_response["result"]["iterations"]
-        optimum_point =  json_response["result"]["optimum_point"]
+        optimum_point = json_response["result"]["optimum_point"]
         optimum_value = json_response["result"]["optimum_value"]
         tables = []
 
@@ -91,151 +91,153 @@ def tabular_view(request):
             tables.append(current_table)
 
         return render(request, 'resultado_tabular.html',
-                      context={"tables":tables, "headers": headers, "optimum_point":optimum_point,
+                      context={"tables": tables, "headers": headers, "optimum_point": optimum_point,
                                "optimum_value": optimum_value})
     except Exception as e:
         print(e)
-        pdb.set_trace()
+        messages.error(request, 'Algo de inesperado aconteceu. Verifique as entradas.')
         return redirect('/')
 
 
 def graphic_view(request):
-    input_data = make_json(request.session)
-    json_request = json.dumps(input_data)
-    json_response = simplex.main.solve_simplex(json_request)
-    output_data = json.loads(json_response)
+    try:
+        input_data = make_json(request.session)
+        json_request = json.dumps(input_data)
+        json_response = simplex.main.solve_simplex(json_request)
+        output_data = json.loads(json_response)
 
-    if len(output_data["error_msg"]) > 0:
         pdb.set_trace()
-        print(output_data)
 
-    pdb.set_trace()
+        if len(output_data["error_msg"]) > 0:
+            pdb.set_trace()
+            print(output_data)
 
-    #Create rescrictions coordinates
-    optimum_point = output_data["result"]["optimum_point"]
-    optimum_value = output_data["result"]["optimum_value"]
-    restrictions = []
-    vertical_rescritions = [0, ]
-    horizontal_restrictions = [0, ]
-    max_x = 0
-    max_y = 0
+        pdb.set_trace()
 
+        # Create rescrictions coordinates
+        optimum_point = output_data["result"]["optimum_point"]
+        optimum_value = output_data["result"]["optimum_value"]
+        restrictions = []
+        vertical_rescritions = [0, ]
+        horizontal_restrictions = [0, ]
+        max_x = 0
+        max_y = 0
 
-    for restrict in input_data["restrictions"][:-len(input_data["variable_names"])]:
-        coord = []
+        for restrict in input_data["restrictions"][:-len(input_data["variable_names"])]:
+            coord = []
 
-        if restrict["coeficients"][0] == 0:
-            horizontal_restrictions.append(restrict["value"] / restrict["coeficients"][1])
-            continue
-        elif restrict["coeficients"][1] == 0:
-            vertical_rescritions.append(restrict["value"] / restrict["coeficients"][0])
-            continue
+            if restrict["coeficients"][0] == 0:
+                horizontal_restrictions.append(restrict["value"] / restrict["coeficients"][1])
+                continue
+            elif restrict["coeficients"][1] == 0:
+                vertical_rescritions.append(restrict["value"] / restrict["coeficients"][0])
+                continue
 
-        # x = 0
-        try:
-            coord.append([0, restrict["value"] / restrict["coeficients"][1]])
-        except:
-            coord.append([0, 0])
-        # y = 0
-        try:
-            coord.append([restrict["value"] / restrict["coeficients"][0], 0])
-        except:
-            coord.append([0, 0])
+            # x = 0
+            try:
+                coord.append([0, restrict["value"] / restrict["coeficients"][1]])
+            except:
+                coord.append([0, 0])
+            # y = 0
+            try:
+                coord.append([restrict["value"] / restrict["coeficients"][0], 0])
+            except:
+                coord.append([0, 0])
 
-        if coord[0][0] > max_x or coord[1][0] > max_x:
-            max_x = max([coord[0][0], coord[1][0]])
-        if coord[0][1] > max_y or coord[1][1] > max_y:
-            max_y = max([coord[0][1], coord[1][1]])
+            if coord[0][0] > max_x or coord[1][0] > max_x:
+                max_x = max([coord[0][0], coord[1][0]])
+            if coord[0][1] > max_y or coord[1][1] > max_y:
+                max_y = max([coord[0][1], coord[1][1]])
 
-        restrictions.append(coord)
+            restrictions.append(coord)
 
+        # Construct DataFrame
+        x = []
+        y = []
+        lables = []
+        values = []
+        for coord in output_data["result"]["points"]:
+            x.append(coord["coords"][0])
+            y.append(coord["coords"][1])
+            lables.append(coord["label"])
+            values.append(coord["value"])
 
-    # Construct DataFrame
-    x = []
-    y = []
-    lables = []
-    values = []
-    for coord in output_data["result"]["points"]:
-        x.append(coord["coords"][0])
-        y.append(coord["coords"][1])
-        lables.append(coord["label"])
-        values.append(coord["value"])
+        data = pd.DataFrame({"x": x, "y": y, "lables": lables, "values": values})
+        sorted_data = data.sort_values(by=['x'])
 
-    data = pd.DataFrame({"x": x, "y": y, "lables": lables, "values": values})
-    sorted_data = data.sort_values(by=['x'])
+        # Create graphic base
+        layout = go.Layout(
+            autosize=True,
+            dragmode="pan",
+            margin=dict({'b': 0, 'l': 0, 'r': 0, 't': 0}))
+        fig = go.Figure(layout=layout)
 
-    # Create graphic base
-    layout = go.Layout(
-        autosize=True,
-        dragmode="pan",
-        margin=dict({'b': 0, 'l':0, 'r':0, 't':0}))
-    fig = go.Figure(layout=layout)
+        # plot restrictions
+        for restrict in restrictions:
+            fig.add_annotation(
+                ax=restrict[0][0], axref='x',
+                ay=restrict[0][1], ayref='y',
+                x=restrict[1][0], xref='x',
+                y=restrict[1][1], yref='y', arrowwidth=3, arrowcolor="red")
 
-    # plot restrictions
-    for restrict in restrictions:
-        fig.add_annotation(
-            ax=restrict[0][0], axref='x',
-            ay=restrict[0][1], ayref='y',
-            x=restrict[1][0],  xref='x',
-            y=restrict[1][1], yref='y', arrowwidth=3, arrowcolor="red")
+        for point in horizontal_restrictions:
+            fig.add_hline(y=point, line_color="red", line_width=3)
+        for point in vertical_rescritions:
+            fig.add_vline(x=point, line_color="red", line_width=3)
 
-    for point in horizontal_restrictions:
-        fig.add_hline(y=point, line_color="red", line_width=3)
-    for point in vertical_rescritions:
-        fig.add_vline(x=point, line_color="red", line_width=3)
+        # plot points and available area
+        fig.add_trace(go.Scatter(x=sorted_data["x"], y=sorted_data["y"], text=sorted_data["lables"],
+                                 mode='lines+markers+text',
+                                 marker_size=[20] * len(x), marker_color="green",
+                                 fill='tonexty', fillcolor='#00CC96', textposition="top right"))
 
+        # plot arrows
+        indexed_data = data.set_index("lables")
+        for i in range(len(output_data["result"]["iterations_path"]) - 1):
+            fig.add_annotation(
+                ax=indexed_data.filter(items=[output_data["result"]["iterations_path"][i]], axis=0)["x"][0], axref='x',
+                ay=indexed_data.filter(items=[output_data["result"]["iterations_path"][i]], axis=0)["y"][0], ayref='y',
+                x=indexed_data.filter(items=[output_data["result"]["iterations_path"][i + 1]], axis=0)["x"][0],
+                xref='x',
+                y=indexed_data.filter(items=[output_data["result"]["iterations_path"][i + 1]], axis=0)["y"][0],
+                yref='y',
+                arrowwidth=3, arrowhead=2)
 
-    # plot points and available area
-    fig.add_trace(go.Scatter(x=sorted_data["x"], y=sorted_data["y"], text=sorted_data["lables"],
-                             mode='lines+markers+text',
-                             marker_size=[20]*len(x), marker_color = "green",
-                             fill='tonexty', fillcolor='#00CC96', textposition="top right"))
+        # Create countour plot lines
+        quant_lines = 20
+        first_mmc = output_data["result"]["optimum_value"]
+        steps = first_mmc / quant_lines
+        contours = []
+        for value in np.arange(0, first_mmc + 1, steps):
+            contour = []
+            # x = 0
+            try:
+                contour.append([0, value / input_data["objective_function"][1]])
+            except:
+                contour.append([0, 0])
+            # y = 0
+            try:
+                contour.append([value / input_data["objective_function"][0], 0])
+            except:
+                contour.append([0, 0])
 
+            contours.append(contour)
 
+        # plot contours
+        for contour in contours:
+            fig.add_annotation(
+                ax=contour[0][0], axref='x',
+                ay=contour[0][1], ayref='y',
+                x=contour[1][0], xref='x',
+                y=contour[1][1], yref='y', arrowwidth=1, arrowcolor="blue")
 
+        config = {'responsive': True}
 
+        graph = plot(fig, output_type="div", config=config)
 
-    # plot arrows
-    indexed_data = data.set_index("lables")
-    for i in range(len(output_data["result"]["iterations_path"]) - 1):
-        fig.add_annotation(ax=indexed_data.filter(items=[output_data["result"]["iterations_path"][i]], axis=0)["x"][0], axref='x',
-                           ay=indexed_data.filter(items=[output_data["result"]["iterations_path"][i]], axis=0)["y"][0], ayref='y',
-                           x=indexed_data.filter(items= [output_data["result"]["iterations_path"][i+1]], axis=0)["x"][0], xref='x',
-                           y=indexed_data.filter(items= [output_data["result"]["iterations_path"][i+1]], axis=0)["y"][0], yref='y',
-                           arrowwidth=3, arrowhead=2)
-
-    #Create countour plot lines
-    quant_lines = 20
-    first_mmc = output_data["result"]["optimum_value"]
-    steps = first_mmc / quant_lines
-    countours = []
-    for value in np.arange(0, first_mmc + 1, steps):
-        countour = []
-        # x = 0
-        try:
-            countour.append([0, value/input_data["objective_function"][1]])
-        except:
-            countour.append([0, 0])
-        #y = 0
-        try:
-            countour.append([value / input_data["objective_function"][0], 0])
-        except:
-            countour.append([0, 0])
-
-        countours.append(countour)
-
-    # plot contourns
-    for countour in countours:
-        fig.add_annotation(
-            ax=countour[0][0], axref='x',
-            ay=countour[0][1], ayref='y',
-            x=countour[1][0],  xref='x',
-            y=countour[1][1], yref='y', arrowwidth=1, arrowcolor="blue")
-
-    config = {'responsive': True}
-
-    graph = plot(fig, output_type="div", config=config)
-
-    return render(request, 'resultado_grafico.html', context={"graph": graph, "optimum_point":optimum_point,
-                           "optimum_value": optimum_value} )
-
+        return render(request, 'resultado_grafico.html', context={"graph": graph, "optimum_point": optimum_point,
+                                                                  "optimum_value": optimum_value})
+    except Exception as e:
+        print(e)
+        messages.error(request, 'Algo de inesperado aconteceu. Verifique as entradas.')
+        return redirect('/')
